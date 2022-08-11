d5682f328b61b7656b4dfa9a687085442ffb4d7e
sMayWhatIsYourName2020 <smaynumberone@gmail.com>
2022-08-11 19:50:49 +0500

    fix display languages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,22 +8,28 @@ import { useSelector } from 'react-redux';
 
 export const Header = (props: HeaderProps): JSX.Element => {
   const { history, languages } = useSelector((state: ITranslation) => state);
-  const favCount = history.filter((lang) => lang.isFavorite).length;  
+  const favCount = history.filter((lang) => lang.isFavorite).length;
   return (
     <header className={styles.header} {...props}>
-      <div className="">
+      <div>
         <h2 className={styles.headling}>Types</h2>
         <ul className={styles.list}>
           <Category icon='layer' name='All items' count={history.length} />
           <Category icon='fav' name='Favorites' count={favCount} />
         </ul>
       </div>
-      <div className="">
-        <h2 className={styles.headling}>Languages</h2>
-        <ul className={cn(styles.list, styles.languages)}>
-          {Object.entries(languages).map(([lang, count]) => <Category icon='folder' key={lang} name={lang} count={count} />)}
-        </ul>
-      </div>
+      {
+        history.length > 0
+          ?
+          <div>
+            <h2 className={styles.headling}>Languages</h2>
+            <ul className={cn(styles.list, styles.languages)}>
+              {Object.entries(languages).map(([lang, count]) => <Category icon='folder' key={lang} name={lang} count={count} />)}
+            </ul>
+          </div>
+          :
+          null
+      }
     </header>
   );
 }
